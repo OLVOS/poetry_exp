fbaaadf53c839e4e0513430b4988ae07a9d6eca4
OLVOS <anhkrem@gmail.com>
2023-11-30 10:57:25 +0300

    all poetry files

---
+++ hello/scripts/say_hello.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Back, Style
+
+
+def main():
+    print(Fore.RED + "Hello!")
+    print(Style.RESET_ALL)
+
+if __name__ == '__main__':
+    main()
+
---
+++ hello/scripts/whoareu.py
@@ -0,0 +1,6 @@
+def main():
+    print("I am Olvos")
+
+
+if __name__ == "__main__":
+    main()
