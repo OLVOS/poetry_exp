f9ef5d430b7bc9da1e627c3460b51749bf252461
OLVOS <anhkrem@gmail.com>
2023-11-30 17:04:08 +0300

    patch say_hello by linter

--- hello/scripts/say_hello.py
+++ hello/scripts/say_hello.py
@@ -1,10 +1,10 @@
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 
 def main():
     print(Fore.RED + "Hello!")
     print(Style.RESET_ALL)
 
+
 if __name__ == '__main__':
     main()
-
